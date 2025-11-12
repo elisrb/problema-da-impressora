09d29bf6f114ae9e0f0b2b61f35372d7d6d77748
Elis Rodrigues <elisrb2004@gmail.com>
2025-11-12 15:35:24 -0300

    cria arquivo do codigo fonte

---
+++ trabalho-pc-codigo.c
@@ -0,0 +1,86 @@
+#include "stdio.h"
+#include "unistd.h"
+#include "stdlib.h"
+#include "pthread.h"
+
+#define TRUE 1
+
+#define NE 10 //numero de escritores
+#define NL 20 //numero de leitores
+
+int rc = 0;                     /* número de processos lendo ou querendo ler */
+
+void * reader(void *arg);
+void * writer(void *arg);
+void read_data_base();
+void use_data_read();
+void think_up_data();
+void write_data_base();
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond_db = PTHREAD_COND_INITIALIZER;
+
+int main() {
+	pthread_t r[NL], w[NE];
+
+	int i;
+        int *id;
+        /* criando leitores */
+    	for (i = 0; i < NL ; i++) {
+	   id = (int *) malloc(sizeof(int));
+           *id = i;
+		 pthread_create(&r[i], NULL, reader, (void *) (id));
+	}
+
+	 /* criando escritores */
+	for (i = 0; i< NE; i++) {
+	   id = (int *) malloc(sizeof(int));
+           *id = i;
+		 pthread_create(&w[i], NULL, writer, (void *) (id));
+	}
+
+	pthread_join(r[0],NULL);
+	return 0;
+}
+
+void* reader(void *arg) {
+	int i = *((int *) arg);
+	while(TRUE) {               /* repere para sempre */
+            
+		 rc = rc + 1;            
+		 read_data_base(i);       
+		 rc = rc - 1;            
+		 use_data_read(i);        /* região não crítica */
+	}
+        pthread_exit(0);
+}
+
+void* writer(void *arg) {
+	int i = *((int *) arg);
+
+	while(TRUE) {               /* repete para sempre */
+		 think_up_data(i);        /* região não crítica */
+		 write_data_base(i);      /* atualiza os dados */
+	}
+        pthread_exit(0);
+}
+
+void read_data_base(int i) {
+	printf("Leitor %d está lendo os dados! Número de leitores = %d \n", i,rc);
+	sleep( rand() % 5);
+}
+
+void use_data_read(int i) {
+	printf("Leitor %d está usando os dados lidos!\n", i);
+	sleep(rand() % 5);
+}
+
+void think_up_data(int i) {
+	printf("Escritor %d está pensando no que escrever!\n", i);
+	sleep(rand() % 5);
+}
+
+void write_data_base(int i) {
+	printf("Escritor %d está escrevendo os dados!\n", i);
+	sleep( rand() % 5 + 5);
+}
